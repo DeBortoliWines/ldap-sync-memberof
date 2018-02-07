b761acf06e40cf822e53e68dc47069cccb0a7da4
Ben Letchford <ben_letchford@debortoli.com.au>
2018-02-07 12:01:34 +1100

    Put some pepper on it.

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python
 
-import sys
-import os
-import argparse
+"""ldap-sync-memberof.
 
+Python script that stores a user's posixGroup memberships against their
+memberOf attribute.
+"""
+
+import argparse
 import ldap3
 
 
 class Ldap():
-    """ Class for containing LDAP connections/operations """
+    """Class for containing LDAP connections/operations."""
 
     def __init__(self, server_uri, ldap_user, ldap_pass):
+        """Constructor."""
         self.server = ldap3.Server(server_uri, get_info=ldap3.ALL)
-        self.conn = ldap3.Connection(self.server, user=ldap_user, password=ldap_pass, auto_bind=True)
+        self.conn = ldap3.Connection(self.server, user=ldap_user,
+                                     password=ldap_pass, auto_bind=True)
 
     def getGroups(self):
-        self.conn.search('dc=debortoli, dc=private', '(objectClass=posixGroup)')
+        """Get all the `posixGroup`s."""
+        self.conn.search('dc=debortoli, dc=private',
+                         '(objectClass=posixGroup)')
         return self.conn.entries
 
     def getGroupsWithMembers(self):
+        """Get dict of groups with member array."""
         fq_groups = [result.entry_dn for result in ldap.getGroups()]
 
         groups_with_members = {}
         for group in fq_groups:
-            self.conn.search(group, '(objectclass=posixGroup)', attributes=['memberUid'])
+            self.conn.search(group, '(objectclass=posixGroup)',
+                             attributes=['memberUid'])
 
             if 'memberUid' in self.conn.entries[0]:
-                groups_with_members[group] = self.conn.entries[0]['memberUid'].values
+                groups_with_members[group] = \
+                    self.conn.entries[0]['memberUid'].values
 
         return groups_with_members
 
     def getMembersWithGroups(self):
+        """Get dict of members with groups array."""
         groups_with_members = self.getGroupsWithMembers()
 
         members_with_groups = {}
         for group, members in groups_with_members.iteritems():
             for member in members:
-                if not member in members_with_groups:
+                if member not in members_with_groups:
                     members_with_groups[member] = []
 
                 members_with_groups[member].append(group)
@@ -44,29 +55,45 @@ class Ldap():
         return members_with_groups
 
     def addObjectClass(self, user, klass):
-        self.conn.search('uid=%s, ou=People, dc=debortoli, dc=private' % user, '(objectclass=inetOrgPerson)', attributes=['objectClass'])
+        """Add an objectClass to user."""
+        self.conn.search(
+            'uid=%s, ou=People, dc=debortoli, dc=private' % user,
+            '(objectclass=inetOrgPerson)',
+            attributes=['objectClass']
+        )
 
         assert len(self.conn.entries) > 0
-        if not klass in self.conn.entries[0]['objectclass'].values:
-            self.conn.modify('uid=%s, ou=People, dc=debortoli, dc=private' % user, {'objectClass': [(ldap3.MODIFY_ADD, [klass])]})
+        if klass not in self.conn.entries[0]['objectclass'].values:
+            self.conn.modify(
+                'uid=%s, ou=People, dc=debortoli, dc=private' % user,
+                {'objectClass': [(ldap3.MODIFY_ADD, [klass])]}
+            )
             return self.conn.result
 
     def addReplaceUserAttributes(self, uid, attributes):
-        self.conn.modify('uid=%s, ou=People, dc=debortoli, dc=private' % uid, attributes)
+        """Add attributes to a user by uid."""
+        self.conn.modify(
+            'uid=%s, ou=People, dc=debortoli, dc=private' % uid,
+            attributes
+        )
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=""" Syncs memberOf attribute onto users (inetOrgPerson) that are in posixGroups. Adds `inetUser` object class to the users if they don't already have it. """)
+    parser = argparse.ArgumentParser(description="Syncs memberOf attribute \
+        onto users (inetOrgPerson) that are in posixGroups. Adds `inetUser` \
+        object class to the users if they don't already have it.")
     parser.add_argument(
         '-D',
         '--user-dn',
-        help='The user DN with correct permissions, such as Directory Manager.',
+        help='The user DN with correct permissions, such as Directory \
+            Manager.',
         default='cn=Directory Manager'
     )
     parser.add_argument(
         '-P',
         '--password-file',
-        help='Password file that has the password in plaintext for the given user.',
+        help='Password file that has the password in plaintext for the given \
+            user.',
         required=True
     )
     parser.add_argument(
@@ -87,8 +114,12 @@ if __name__ == '__main__':
         try:
             ldap.addObjectClass(member, 'inetUser')
         except AssertionError:
-            print 'memberUid of %s does not have object class inetOrgPerson so not syncing.' % member
+            print 'memberUid of %s does not have object class inetOrgPerson so \
+                not syncing.' % member
             continue
 
-        ldap.addReplaceUserAttributes(uid=member, attributes={'memberOf': [(ldap3.MODIFY_REPLACE, groups)]})
+        ldap.addReplaceUserAttributes(uid=member, attributes={
+                'memberOf': [(ldap3.MODIFY_REPLACE, groups)]
+            }
+        )
         print 'Synced memberOf for %s' % member
